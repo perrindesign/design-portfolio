ba59a702dd9437e96373aac692a0236bf8cffa51
perrindesign <p.jones@columbia.edu>
2018-10-13 14:38:32 +1100

    Input content, finished main page mostly, and started animations

---
+++ js/script.js
@@ -0,0 +1,12 @@
+
+$(document).ready(function() {
+    var body = $('body');
+
+    $(function(){
+        if(body.hasClass('.black-background')){
+            //TweenLite.to(body, 10, { backgroundColor: "#FF0000" }); // background color
+        } else {
+            //TweenLite.to(body, 2, { backgroundColor: "#FF0000" }); // background color
+        }
+      });
+});
