19c77a3068ff300bb06c320fb4b3ec7f92206130
perrindesign <p.jones@columbia.edu>
2018-10-13 20:04:32 +1100

    Added resume and continued to fix animations

--- js/script.js
+++ js/script.js
@@ -1,16 +1,19 @@
 
 $(document).ready(function() {
-    var body = $('body');
-    var core_content = $('.core-content');
 
-    var fadein_tween = TweenLite.from(core_content, 1, {autoAlpha:0, y: 100});
- 
+});
 
-    $(function(){
-        if(body.hasClass('.black-background')){
-            //TweenLite.to(body, 10, { backgroundColor: "#FF0000" }); // background color
-        } else {
-            //TweenLite.to(body, 2, { backgroundColor: "#FF0000" }); // background color
-        }
-      });
+// wait until DOM is ready
+document.addEventListener("DOMContentLoaded", function(event) {
+
+    // wait until window, stylesheets, images, links, and other media assets are loaded
+    window.onload = function() {
+        
+        document.getElementById("container").style.display = "block";
+        var $core_content = $('.core-content');
+
+        var tl = new TimelineLite();
+        tl.from($core_content, 1, {autoAlpha:0, y: 100, delay: .5, ease:Power4.easeInOut});
+
+    };
 });
