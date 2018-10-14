cd1b6dbe192d6586674773f46254ccf8f37ad15c
perrindesign <p.jones@columbia.edu>
2018-10-14 17:17:57 +1100

    Fixed animation loading in

--- js/script.js
+++ js/script.js
@@ -1,8 +1,11 @@
-//For AOS animation
-AOS.init();
-
 //For loading bar animation
 $(document).ready(function() {
+    //For AOS animation
+    AOS.init({
+        easing: 'ease-in',
+        duration: 1000
+    });
+    
     var simplebar = new Nanobar();
     simplebar.go(50);
     simplebar.go(100);
