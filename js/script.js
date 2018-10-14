e9026f782212e9c66e3bd63cd152bbce6ffb5e5e
perrindesign <p.jones@columbia.edu>
2018-10-14 17:10:58 +1100

    Changed from greensock to aos.js

--- js/script.js
+++ js/script.js
@@ -1,19 +1,8 @@
+AOS.init();
+//For AOS animation
 
 $(document).ready(function() {
-
-});
-
-// wait until DOM is ready
-document.addEventListener("DOMContentLoaded", function(event) {
-
-    // wait until window, stylesheets, images, links, and other media assets are loaded
-    window.onload = function() {
-        
-        document.getElementById("container").style.display = "block";
-        var $core_content = $('.core-content');
-
-        var tl = new TimelineLite();
-        tl.from($core_content, 1, {autoAlpha:0, y: 100, delay: .5, ease:Power4.easeInOut});
-
-    };
+    var simplebar = new Nanobar();
+    simplebar.go(50);
+    simplebar.go(100);
 });
